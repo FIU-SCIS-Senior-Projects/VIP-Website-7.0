1b6f977ce0005326cd7b5857698f6165fc5e3b7f
Yizue <Yizue>
2017-09-26 17:36:19 -0400

    Added API for Terms

---
+++ Code/api/routes/termRoutes.js
@@ -0,0 +1,119 @@
+var Term = require('../models/terms');
+var authProvider = require('../services/AuthorizationProvider');
+
+module.exports = function (app, express) {
+    var apiRouter = express.Router();
+	
+	apiRouter.route('/terms')
+		.post(
+			authProvider.authorizeByUserType(authProvider.userType.PiCoPi),
+            function (req, res) {
+                Term.create(req.body, function (err) {
+                    if (err) {
+                        return res.send('Error found');
+                    } else {
+                        res.send('Term has been added');
+                    }
+                });
+            }
+		)
+		.get( // Get all the open terms
+			authProvider.authorizeAuthenticatedUsers,
+            function (req, res) {
+                Term.find({status: 'open'}, function (err, terms) {
+                    if (err) {
+                        return res.send(err);
+                    }
+                    return res.json(terms);
+                });
+            }
+		);
+		
+	apiRouter.route('/terms/:id')
+		.get(
+            authProvider.authorizeAuthenticatedUsers,
+            function (req, res) {
+                Term.findById(req.params.id, function (err, term) {
+                    if (err)
+                        return res.send(err);
+                    res.json(term);
+                });
+            })
+        .put(
+            authProvider.authorizeByUserType(authProvider.userType.PiCoPi),//it doesn't seem to be needed by any other users
+            function (req, res) {
+                 Term.findById(req.params.id, function (err, term) {
+                    if (err) {
+                        res.status(400);
+                        res.send(err);
+                    }
+					
+					term.name = req.body.name;
+                    term.status = req.body.status;
+					term.start = req.body.start;
+                    term.end = req.body.end;
+                    term.active = req.body.active;
+                    
+                    term.save(function (err) {
+                        if (err) {
+                            res.status(400);
+                            return res.send(err);
+                        }
+                        res.json({message: 'Updated!'});
+                    });
+                });
+            })
+        .delete(
+            authProvider.authorizeByUserType(authProvider.userType.PiCoPi),
+            function (req, res) {
+                Term.remove({_id: req.params.id}, function (err, term) {
+                    if (err)
+                        return res.send(err);
+                    res.json({message: 'successfully deleted!'});
+                });
+            });
+			
+	apiRouter.route('/terms/:name')
+		.get(
+            authProvider.authorizeAuthenticatedUsers,
+            function (req, res) {
+                Term.findOne({name: req.params.name}, function (err, term) {
+                    if (err)
+                        return res.send(err);
+                    res.json(term);
+                });
+            })
+        .put(
+            authProvider.authorizeByUserType(authProvider.userType.PiCoPi),//it doesn't seem to be needed by any other users
+            function (req, res) {
+                 Term.findOne({name: req.params.name}, function (err, term) {
+                    if (err) {
+                        res.status(400);
+                        res.send(err);
+                    }
+					
+					term.name = req.body.name;
+                    term.status = req.body.status;
+					term.start = req.body.start;
+                    term.end = req.body.end;
+                    term.active = req.body.active;
+                    
+                    term.save(function (err) {
+                        if (err) {
+                            res.status(400);
+                            return res.send(err);
+                        }
+                        res.json({message: 'Updated!'});
+                    });
+                });
+            })
+        .delete(
+            authProvider.authorizeByUserType(authProvider.userType.PiCoPi),
+            function (req, res) {
+                Term.remove({name: req.params.name}, function (err, term) {
+                    if (err)
+                        return res.send(err);
+                    res.json({message: 'successfully deleted!'});
+                });
+            });
+}

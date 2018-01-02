0a2551a78378e2361bcb218d34b20c3e8e8f0855
Dale Keith <dkeit008@fiu.edu>
2018-01-02 14:02:46 -0500

    One more update to auto renew script.
    This works now. But the server may need to manually be restarted.

--- Code/certificateRenewal.bash
+++ Code/certificateRenewal.bash
@@ -6,9 +6,9 @@ VIP_WEB_DOMAIN="vip-dev.cis.fiu.edu"
 
 /var/www/certbot-auto renew | grep 'No renewals were attempted.'
 if [ $? -ne 0 ]; then
-    cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/privkey.pem" "${SERVERJS_LOCATION}/"
-    cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/cert.pem" "${SERVERJS_LOCATION}/"
-    cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/chain.pem" "${SERVERJS_LOCATION}/"
+    sudo cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/privkey.pem" "${SERVERJS_LOCATION}/"
+    sudo cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/cert.pem" "${SERVERJS_LOCATION}/"
+    sudo cp "/etc/letsencrypt/live/${VIP_WEB_DOMAIN}/chain.pem" "${SERVERJS_LOCATION}/"
 
     #restart the server
     kill $(pidof nodejs)
